2fcfebf63adef2eda1372a93a960fb53ae75da66
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2023-12-02 19:26:42 +0300

    Init mock code

--- src/web/ui_inference.py
+++ src/web/ui_inference.py
@@ -2,3 +2,19 @@
 
 This is main file for UI inference developed using Streamlit framework.
 """
+import streamlit as st
+
+
+model = ...  # singleton model instance
+photo = st.camera_input(label='take a photo')
+if photo:
+    processed_photo = ...  # preprocess photo before pass it to neural net
+    embedding = ...
+    result = '<Имя пользователя>'  # call specialized methods to get name with embedding
+    if result:
+        st.write(result)
+    else:
+        full_name = st.text_input(label='ФИО:')
+        if st.button('Зарегистрировать'):
+            # register new user
+            pass
