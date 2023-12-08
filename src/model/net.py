abfbcb5d63e672e6862610c3f022fa0cf19df6b2
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2023-12-08 09:36:51 +0300

    Complete web ui

--- src/model/net.py
+++ src/model/net.py
@@ -2,3 +2,10 @@
 
 This file contains neural net architecture.
 """
+import face_recognition
+
+
+def compute_embedding(img_path):
+    image = face_recognition.load_image_file(img_path)
+    encoding = face_recognition.face_encodings(image)[0]
+    return encoding.tolist()
--- src/web/ui_inference.py
+++ src/web/ui_inference.py
@@ -2,19 +2,27 @@
 
 This is main file for UI inference developed using Streamlit framework.
 """
+import sys
+
 import streamlit as st
+from PIL import Image
+
+sys.path.append('C:\\Users\\melik\\PycharmProjects\\face-recognition\\src')
+
+from model.net import compute_embedding
+from data.dataset import FacesData
 
 
-model = ...  # singleton model instance
+faces_data = FacesData()
 photo = st.camera_input(label='take a photo')
 if photo:
-    processed_photo = ...  # preprocess photo before pass it to neural net
-    embedding = ...
-    result = '<Имя пользователя>'  # call specialized methods to get name with embedding
+    image = Image.open(photo)  # preprocess photo before pass it to neural net
+    image.save('src/data/photo.png')
+    embedding = compute_embedding('src/data/photo.png')
+    result = faces_data.get_most_similar(embedding)  # call specialized methods to get name with embedding
     if result:
         st.write(result)
     else:
         full_name = st.text_input(label='ФИО:')
         if st.button('Зарегистрировать'):
-            # register new user
-            pass
+            faces_data.add(full_name, embedding)
