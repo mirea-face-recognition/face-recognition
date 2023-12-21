b1436b5eae9d36761333a02177b14bf8d0b11da4
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2023-12-22 01:00:27 +0300

    Integrate text2speech

--- src/config.py
+++ src/config.py
@@ -4,3 +4,9 @@ This file contains constants used in training process.
 """
 FACES_JSON = 'src/data/registered_faces.json'
 RECOGNITION_THRESHOLD = 0.7
+
+# text2speech config
+LANGUAGE = 'ru'
+MODEL_ID = 'v4_ru'
+SAMPLE_RATE = 48000
+SPEAKER = 'kseniya'
