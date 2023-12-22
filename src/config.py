b59e7a4a952b9e4dfab61ef60bf325bbfdb453b0
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2023-12-22 17:29:53 +0300

    fix errors, make faster text2speech

--- src/config.py
+++ src/config.py
@@ -2,11 +2,11 @@
 
 This file contains constants used in training process.
 """
-FACES_JSON = 'src/data/registered_faces.json'
+FACES_JSON = 'data/registered_faces.json'
 RECOGNITION_THRESHOLD = 0.7
 
 # text2speech config
 LANGUAGE = 'ru'
 MODEL_ID = 'v4_ru'
-SAMPLE_RATE = 48000
+SAMPLE_RATE = 24000
 SPEAKER = 'kseniya'
